60f592b26f44d81b55d436da7b65fcbc0afeed0b
romzessss <106191086+romzessss@users.noreply.github.com>
2022-10-31 02:04:19 +0300

    init

---
+++ app.js
@@ -0,0 +1,50 @@
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+for (placeholder of placeholders) {
+	placeholder.addEventListener('dragover', dragover)
+	placeholder.addEventListener('dragenter', dragenter)
+	placeholder.addEventListener('dragleave', dragleave)
+	placeholder.addEventListener('drop', dragdrop)
+}
+
+function dragstart(event) {
+	event.target.classList.add('hold')
+	setTimeout(() =>
+		event.target.classList.add('hide'), 0
+	)
+
+
+}
+function dragend(event) {
+
+	event.target.classList.remove('hold', 'hide')
+
+}
+
+function dragover(event) {
+	event.preventDefault()
+	console.log('drag over')
+
+}
+function dragenter(event) {
+	event.target.classList.add('hovered')
+	console.log('drag enter')
+
+}
+function dragleave(event) {
+	event.target.classList.remove('hovered')
+	console.log('drag leave')
+
+}
+function dragdrop(event) {
+	event.target.append(item)
+	console.log('drop')
+	event.target.classList.remove('hovered')
+
+}
